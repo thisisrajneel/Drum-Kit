00c6eafa57401eb6f51849fbdebbc6ad75b7362a
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-06-26 23:45:23 +0530

    trying to play sounds

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 for (let i = 0; i < document.getElementsByClassName("drum").length; i++) {
   document.getElementsByClassName("drum")[i].addEventListener("click", () => {
-    this.style.color = "white";
+    switch (this.innerHTML) {
+      case "w":
+        let sound = new Audio("sounds/tom-1.mp3");
+        sound.play();
+        break;
+
+      case "a":
+        let sound = new Audio("sounds/tom-2.mp3");
+        sound.play();
+        break;
+
+      case "s":
+        let sound = new Audio("sounds/tom-3.mp3");
+        sound.play();
+        break;
+
+      case "d":
+        let sound = new Audio("sounds/tom-4.mp3");
+        sound.play();
+        break;
+
+      case "j":
+        let sound = new Audio("sounds/snare.mp3");
+        sound.play();
+        break;
+
+      case "k":
+        let sound = new Audio("sounds/crash.mp3");
+        sound.play();
+        break;
+
+      case "l":
+        let sound = new Audio("sounds/kick-bass.mp3");
+        sound.play();
+        break;
+    }
   });
 }
-
-// let sound = new Audio('sounds/kick-bass.mp3');
-//     sound.play();
