2c52d0b643b79778f63f767e76c00d54c17b7206
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-06-26 23:09:22 +0530

    changing bg color

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 for (let i = 0; i < document.getElementsByClassName("drum").length; i++) {
   document.getElementsByClassName("drum")[i].addEventListener("click", () => {
-    let sound = new Audio('sounds/kick-bass.mp3');
-    sound.play();
+    this.setAttribute('color', '#fff');
   });
 }
+
+// let sound = new Audio('sounds/kick-bass.mp3');
+//     sound.play();
