96067e444acf920633231c0d72baf807a81d97ec
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-06-27 11:19:46 +0530

    added keyboard event listeners, made listening function

--- src/index.js
+++ src/index.js
@@ -1,40 +1,48 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    switch (this.innerHTML) {
-      case "w":
-        var sound = new Audio("sounds/tom-1.mp3");
-        sound.play();
-        break;
-
-      case "a":
-        var sound = new Audio("sounds/tom-2.mp3");
-        sound.play();
-        break;
-
-      case "s":
-        var sound = new Audio("sounds/tom-3.mp3");
-        sound.play();
-        break;
-
-      case "d":
-        var sound = new Audio("sounds/tom-4.mp3");
-        sound.play();
-        break;
-
-      case "j":
-        var sound = new Audio("sounds/snare.mp3");
-        sound.play();
-        break;
-
-      case "k":
-        var sound = new Audio("sounds/crash.mp3");
-        sound.play();
-        break;
-
-      case "l":
-        var sound = new Audio("sounds/kick-bass.mp3");
-        sound.play();
-        break;
-    }
+    makeSound(this.innerHTML);
   });
+}
+
+document.addEventListener('keypress',function(event) {
+  makeSound(event.key);
+});
+
+const makeSound = (temp) => {
+  switch (temp) {
+    case "w":
+      var sound = new Audio("sounds/tom-1.mp3");
+      sound.play();
+      break;
+
+    case "a":
+      var sound = new Audio("sounds/tom-2.mp3");
+      sound.play();
+      break;
+
+    case "s":
+      var sound = new Audio("sounds/tom-3.mp3");
+      sound.play();
+      break;
+
+    case "d":
+      var sound = new Audio("sounds/tom-4.mp3");
+      sound.play();
+      break;
+
+    case "j":
+      var sound = new Audio("sounds/snare.mp3");
+      sound.play();
+      break;
+
+    case "k":
+      var sound = new Audio("sounds/crash.mp3");
+      sound.play();
+      break;
+
+    case "l":
+      var sound = new Audio("sounds/kick-bass.mp3");
+      sound.play();
+      break;
+  }
 }
