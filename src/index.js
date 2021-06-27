47f1dda84e088d51501f080a9cd1bb27e7395ea7
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-06-27 10:06:08 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.gquerySelectorAll(".drum")[i].addEventListener("click", () => {
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
     switch (this.innerHTML) {
       case "w":
-        let sound = new Audio("sounds/tom-1.mp3");
+        var sound = new Audio("sounds/tom-1.mp3");
         sound.play();
         break;
 
       case "a":
-        let sound = new Audio("sounds/tom-2.mp3");
+        var sound = new Audio("sounds/tom-2.mp3");
         sound.play();
         break;
 
       case "s":
-        let sound = new Audio("sounds/tom-3.mp3");
+        var sound = new Audio("sounds/tom-3.mp3");
         sound.play();
         break;
 
       case "d":
-        let sound = new Audio("sounds/tom-4.mp3");
+        var sound = new Audio("sounds/tom-4.mp3");
         sound.play();
         break;
 
       case "j":
-        let sound = new Audio("sounds/snare.mp3");
+        var sound = new Audio("sounds/snare.mp3");
         sound.play();
         break;
 
       case "k":
-        let sound = new Audio("sounds/crash.mp3");
+        var sound = new Audio("sounds/crash.mp3");
         sound.play();
         break;
 
       case "l":
-        let sound = new Audio("sounds/kick-bass.mp3");
+        var sound = new Audio("sounds/kick-bass.mp3");
         sound.play();
         break;
     }
